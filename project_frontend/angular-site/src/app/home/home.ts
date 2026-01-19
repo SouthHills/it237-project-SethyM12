d238928e6249138dd3e36fcdca9253290d724fce
AlexSawyer10 <asawyer37@southhills.edu>
2026-01-19 15:40:16 -0500

    First home page design, and added a gitIgnore file

--- project_frontend/angular-site/src/app/home/home.ts
+++ project_frontend/angular-site/src/app/home/home.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
+  standalone: true
 })
 export class Home {
 
