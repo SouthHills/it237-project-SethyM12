faa600abb156c44eb6a52f27c0abaac239be564a
AlexSawyer10 <asawyer37@southhills.edu>
2026-02-18 01:12:41 -0500

    dont think we need the imports but doesnt hurt to keep

--- project_frontend/angular-site/src/app/app.config.ts
+++ project_frontend/angular-site/src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay()),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
   ]
 };
