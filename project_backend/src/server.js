02cedee13862504b202797fdf1774f3e028e53a8
SethyM12 <seth50821@outlook.com>
2026-03-03 19:46:56 -0500

    Dockerfile fixes, ran on Azure

--- project_backend/src/server.js
+++ project_backend/src/server.js
@@ -9,7 +9,7 @@ import { vendorRouter } from "./routes/vendorRoutes.js";
 import { plantRouter } from "./routes/plantRoutes.js";
 import { buildRouter } from "./routes/buildRoutes.js";
 export const app = express();
-const port = 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/components", componentRouter);
