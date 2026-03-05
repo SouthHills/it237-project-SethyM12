61ab659a5c7ff51e81502e6ebc070e0a36001b4d
SethyM12 <seth50821@outlook.com>
2026-03-05 11:26:09 -0500

    Hoping for the best

--- project_frontend/angular-site/src/environments/environment.prod.ts
+++ project_frontend/angular-site/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://your-backend-app.azurecontainerapps.io'  // Replace with your actual Azure backend URL
+  apiBaseUrl: 'https://backendapp1000-hccpe9dufmhgbgbr.eastus2-01.azurewebsites.net/'  // Replace with your actual Azure backend URL
 };
